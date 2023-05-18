474ee605d36e3906ca280cfd681bd628db97e249
YusufHasan94 <yhpolok@gmail.com>
2023-05-18 22:06:18 +0600

    adding banner

--- src/Layout/Banner/Banner.jsx
+++ src/Layout/Banner/Banner.jsx
@@ -1,35 +1,48 @@
-import banner3 from "../../assets/banner/banner3.png";
-import banner4 from "../../assets/banner/banner4.png";
+import banner from "../../assets/banner.png";
+import banner2 from "../../assets/banner2.png";
 const Banner = () => {
     return (
-        <div className="carousel w-full">
-            <div id="slide1" className="carousel-item relative w-full bg-slate-900">
-                <img src={banner3} className="w-full" />
-                <div className="absolute w-full flex justify-center top-1/4">
-                    <div className="w-1/2 text-white text-xl">
-                        <h1 className="text-4xl font-bold">Toy for Play</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate neque voluptas quam recusandae esse laboriosam minima ipsa quaerat voluptates a?</p>
+        <div className="carousel w-full my-10 rounded-xl">
+            <div id="slide1" className="py-4 bg-gradient-to-r from-indigo-500 to-purple-500  carousel-item relative w-full">
+                <div className="w-full flex justify-center items-center md:mx-24">
+                    <div className="w-1/2 flex justify-center items-center ">
+                        <div className="text-white p-8">
+                                <h1 className="text-white text-6xl mb-4">It's Time To Play</h1>
+                                <p className="text-xl">Play gives children a chance to practice what they are learning.</p>
+                        </div>
+                    </div>
+                    <div className="w-1/2 flex justify-center items-center ">
+                        <div>
+                            <img src={banner} alt="" />
+                        </div>
                     </div>
                 </div>
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">❮</a> 
-                    <a href="#slide2" className="btn btn-circle">❯</a>
+                <div className="absolute flex justify-center gap-2 transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide2" className="btn btn-circle bg-purple-400 border-0">❮</a> 
+                    <a href="#slide2" className="btn btn-circle bg-purple-400 border-0">❯</a>
                 </div>
-            </div> 
-            <div id="slide2" className="carousel-item relative w-full">
-                <img src={banner4} className="w-full" />
-                <div className="absolute w-full flex justify-center top-1/4">
-                    <div className="w-1/2 text-white text-xl">
-                        <h1 className="text-4xl font-bold">Toy for Play</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate neque voluptas quam recusandae esse laboriosam minima ipsa quaerat voluptates a?</p>
+            </div>
+            <div id="slide2" className="py-4 bg-gradient-to-r from-purple-500 to-indigo-500 carousel-item relative w-full">
+                <div className="w-full flex justify-center items-center md:mx-24">
+                    <div className="w-1/2 flex justify-center items-center ">
+                        <div className="text-white p-8">
+                                <h1 className="text-white text-6xl mb-4">Kids Car Zone</h1>
+                                <p className="text-xl">Children have always learned and created places for themselves through play.</p>
+                        </div>
+                    </div>
+                    <div className="w-1/2 flex justify-center items-center ">
+                        <div>
+                            <img src={banner2} alt="" />
+                        </div>
                     </div>
                 </div>
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">❮</a> 
-                    <a href="#slide1" className="btn btn-circle">❯</a>
+                <div className="absolute flex justify-center gap-2 transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide1" className="btn btn-circle bg-purple-400 border-0">❮</a> 
+                    <a href="#slide1" className="btn btn-circle bg-purple-400 border-0">❯</a>
                 </div>
             </div>
         </div>
+        
     );
 };
 
